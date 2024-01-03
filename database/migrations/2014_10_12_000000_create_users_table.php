0e51a74a5e6970ecc080930cfa950ca092291e23
Mo Khosh <mskhoshnazar@gmail.com>
2024-01-03 18:17:14 +0330

    add status and substatus to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('status')->nullable();
+            $table->string('substatus')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
