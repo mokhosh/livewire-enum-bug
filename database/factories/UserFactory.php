e93978a90b13881e64e662790ee9338330b2d831
Mo Khosh <mskhoshnazar@gmail.com>
2024-01-03 18:30:28 +0330

    add status and substatus to factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enums\UserStatus;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -23,11 +24,16 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $status = $this->faker->randomElement(UserStatus::values());
+        $substatus = $this->faker->randomElement(UserStatus::substatuses($status));
+
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'status' => $status,
+            'substatus' => $substatus,
             'remember_token' => Str::random(10),
         ];
     }
